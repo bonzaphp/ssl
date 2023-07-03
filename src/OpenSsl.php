8615b6241f6a95ad42d82de9d565aa0f640c99a4
bonza <bonzaphp@gmail.com>
2023-07-03 15:04:35 +0800

    新增pkcs格式转换为pem格式方法 完善注释与说明，并新增私钥分段解密

--- src/OpenSsl.php
+++ src/OpenSsl.php
@@ -18,6 +18,14 @@ namespace bonza\ssl;
 use bonza\ssl\exception\RuntimeException;
 use Exception;
 
+/**
+ * openssl操作库，包括生成密钥文件，密钥字符，密钥转换等
+ * Class OpenSsl
+ * 类初始化的时候，就会生成密钥对
+ * @author bonzaphp@gmail.com
+ * @Date 2023/7/3 14:47
+ * @package bonza\ssl
+ */
 class OpenSsl
 {
     /**
@@ -33,7 +41,7 @@ class OpenSsl
      *     'private_key_type' => OPENSSL_KEYTYPE_RSA,
      * ];
      * OpenSsl constructor.
-     * @param  array  $config
+     * @param array $config
      */
     public function __construct(array $config)
     {
@@ -41,7 +49,13 @@ class OpenSsl
             throw new RuntimeException('openssl not load');
         }
         // 生成私钥对象
-        $this->privateKey = openssl_pkey_new($config);
+        //必须安装有效的 openssl.cnf 以保证此函数正确运行
+        $res = openssl_pkey_new($config);
+        if (false === $this->privateKey) {
+            throw new RuntimeException('初始化生成私钥失败');
+        }
+        $this->privateKey = $res;
+
     }
 
     /**
@@ -58,7 +72,7 @@ class OpenSsl
     }
 
     /**
-     * 生成公钥
+     * 根据初始化生成的私钥,生成公钥
      * @return mixed
      * @author bonzaphp@gmail.com
      */
@@ -73,8 +87,8 @@ class OpenSsl
 
     /**
      * 保存秘钥到文件
-     * @param  string  $dir  目录地址
-     * @param  string  $file_name  文件名称
+     * @param string $dir 目录地址
+     * @param string $file_name 文件名称
      * @author bonzaphp@gmail.com
      */
     public function generateKey(string $dir = '', string $file_name = 'id_rsa'): void
@@ -83,29 +97,29 @@ class OpenSsl
             $dir = realpath('.');
         }
         if (!is_dir($dir) || !is_writable($dir)) {
-            throw new RuntimeException($dir.'目录无法写入');
+            throw new RuntimeException($dir . '目录无法写入');
         }
-        $handle = fopen($dir.$file_name.'.log', 'wb+');
+        $handle = fopen($dir . $file_name . '.log', 'wb+');
         if (false === $handle) {
-            throw new RuntimeException($dir.'写入文件错误');
+            throw new RuntimeException($dir . '写入文件错误');
         }
         fclose($handle);
         rtrim($dir, DIRECTORY_SEPARATOR);
-        if (!file_exists($dir.DIRECTORY_SEPARATOR.$file_name)) {
-            file_put_contents($dir.DIRECTORY_SEPARATOR.$file_name, $this->generatePrivateKey());
+        if (!file_exists($dir . DIRECTORY_SEPARATOR . $file_name)) {
+            file_put_contents($dir . DIRECTORY_SEPARATOR . $file_name, $this->generatePrivateKey());
         }
-        if (!file_exists($dir.DIRECTORY_SEPARATOR.$file_name.'.pub')) {
-            file_put_contents($dir.DIRECTORY_SEPARATOR.$file_name.'.pub', $this->generatePublicKey());
+        if (!file_exists($dir . DIRECTORY_SEPARATOR . $file_name . '.pub')) {
+            file_put_contents($dir . DIRECTORY_SEPARATOR . $file_name . '.pub', $this->generatePublicKey());
         }
     }
 
     /**
      * 获取可用的私钥内容
-     * @param  string  $private_key  私钥PEM格式
+     * @param string $private_key 私钥PEM格式
      * @return resource
      * @author bonzaphp@gmail.com
      */
-    public function getPrivateKey(string $private_key)
+    protected function getPrivateKey(string $private_key)
     {
         $privateKey = openssl_pkey_get_private($private_key);
         if (false === $privateKey) {
@@ -116,11 +130,11 @@ class OpenSsl
 
     /**
      * 获取可用的公钥内容
-     * @param  string  $public_key  公钥PEM格式
+     * @param string $public_key 公钥PEM格式
      * @return resource
      * @author bonzaphp@gmail.com
      */
-    public function getPublicKey(string $public_key)
+    protected function getPublicKey(string $public_key)
     {
         $publicKey = openssl_pkey_get_public($public_key);
         if (false === $publicKey) {
@@ -131,33 +145,56 @@ class OpenSsl
 
     /**
      * 解密公钥加密的字符串
-     * @param  string  $private_key  私钥字符串
-     * @param  string  $encrypted_key  要解密的字符串
+     * @param string $private_key 私钥字符串
+     * @param string $encrypted_key 要解密的字符串
      * @param  $split_len
      * @return string
      * @author bonzaphp@gmail.com
      */
     public function decodeByPrivateKey(string $private_key, string $encrypted_key, $split_len = 128): string
     {
-        $privateKey = openssl_pkey_get_private($private_key);
-        $encrypted_key_source = base64_decode($encrypted_key);
-        $encrypted_str = str_split($encrypted_key_source, $split_len);
-        $decrypted = '';
-        foreach ($encrypted_str as $val) {
-            $decrypted_temp = '';
-            $res = openssl_private_decrypt($val, $decrypted_temp, $privateKey, OPENSSL_PKCS1_PADDING);
-            if (false === $res) {
-                throw new RuntimeException('解密失败');
+        try {
+            $privateKey = $this->getPrivateKey($private_key);
+            $encrypted_key_source = base64_decode($encrypted_key);
+            $encrypted_str = str_split($encrypted_key_source, $this->getDecryptBlockLen($privateKey));
+            $decrypted = '';
+            foreach ($encrypted_str as $val) {
+                $decrypted_temp = '';
+                $res = openssl_private_decrypt($val, $decrypted_temp, $privateKey, OPENSSL_PKCS1_PADDING);
+                if (false === $res) {
+                    throw new RuntimeException('解密失败');
+                }
+                $decrypted .= $decrypted_temp;
             }
-            $decrypted .= $decrypted_temp;
+            return $decrypted;
+        } catch (Exception $e) {
+            throw new RuntimeException($e->getMessage());
         }
-        return $decrypted;
+    }
+
+    /**
+     * 获取密钥分段解密长度.
+     *
+     * @param $keyClosure
+     *
+     * @return int
+     *
+     * @throws Exception
+     */
+    protected function getDecryptBlockLen($keyClosure)
+    {
+        $key_info = openssl_pkey_get_details($keyClosure);
+        if (!$key_info) {
+            throw new RuntimeException('获取密钥信息失败' . openssl_error_string());
+        }
+        // bits数除以8得到字符长度
+        return $key_info['bits'] / 8;
     }
 
     /**
      * 解密公钥加密的字符串
-     * @param  string  $public_key_str  公钥字符串
-     * @param  string  $encrypted_key  要解密的字符串
+     * @param string $public_key_str 公钥字符串
+     * @param string $encrypted_key 要解密的字符串
      * @return string
      * @author bonzaphp@gmail.com
      */
@@ -173,50 +210,59 @@ class OpenSsl
 
     /**
      * 使用公钥加密字符串
-     * @param  string  $public_key  公钥字符串
-     * @param  string  $str  要加密的字符串
+     * 这里的公钥格式，必须是pem格式的，如果不是，需要先转换
+     * @param string $public_key 公钥字符串
+     * @param string $str 要加密的字符串
      * @return string
      * @author bonzaphp@gmail.com
      */
     public function encodeByPublicKey(string $public_key, string $str): string
     {
-        $publicKey = openssl_pkey_get_public($public_key);
-        $key_len = openssl_pkey_get_details($publicKey)['bits'];
-        $encrypted_str = str_split($str, $key_len / 8 - 11);
-        $encrypted = '';
-        foreach ($encrypted_str as $val) {
-            $encrypted_temp = '';
-            $res = openssl_public_encrypt($val, $encrypted_temp, $publicKey, OPENSSL_PKCS1_PADDING);
-            if (false === $res) {
-                throw new RuntimeException('加密失败');
+        try {
+            $publicKey = $this->getPublicKey($public_key);
+            $encrypted_str = str_split($str, $this->getDecryptBlockLen($publicKey) - 11);
+            $encrypted = '';
+            foreach ($encrypted_str as $val) {
+                $encrypted_temp = '';
+                $res = openssl_public_encrypt($val, $encrypted_temp, $publicKey, OPENSSL_PKCS1_PADDING);
+                if (false === $res) {
+                    throw new RuntimeException('加密失败');
+                }
+                $encrypted .= $encrypted_temp;
             }
-            $encrypted .= $encrypted_temp;
+            return base64_encode($encrypted);
+        } catch (Exception $e) {
+            throw new RuntimeException($e->getMessage());
         }
-        return base64_encode($encrypted);
     }
 
     /**
      * 使用私钥加密字符串
-     * @param  string  $private_key_str  私钥字符串
-     * @param  string  $str  要加密的字符串
+     * 私钥必须是pem格式
+     * @param string $private_key 私钥字符串
+     * @param string $str 要加密的字符串
      * @return string
      * @author bonzaphp@gmail.com
      */
-    public function encodeByPrivateKey(string $private_key_str, string $str): string
+    public function encodeByPrivateKey(string $private_key, string $str): string
     {
-        $private_key = openssl_pkey_get_private($private_key_str);
-        $res = openssl_private_encrypt($str, $encrypted, $private_key);
-        if (false === $res) {
-            throw new RuntimeException('加密失败');
+        try {
+            $privateKey = $this->getPrivateKey($private_key);
+            $res = openssl_private_encrypt($str, $encrypted, $privateKey);
+            if (false === $res) {
+                throw new RuntimeException('加密失败');
+            }
+            return $encrypted;
+        } catch (Exception $e) {
+            throw new RuntimeException($e->getMessage());
         }
-        return $encrypted;
     }
 
     /**
      * 对称加密
-     * @param  string  $data  要加密的数据
-     * @param  string  $key  加密秘钥
-     * @param  string  $encryptMethod  加密算法
+     * @param string $data 要加密的数据
+     * @param string $key 加密秘钥
+     * @param string $encryptMethod 加密算法
      * @return array
      * @author bonzaphp@gmail.com
      */
@@ -229,7 +275,7 @@ class OpenSsl
             $encrypt_str = openssl_encrypt($data, $encryptMethod, $key, 0, $iv);
             return [
                 'encrypt_str' => $encrypt_str,
-                'iv'          => $iv
+                'iv'          => $iv,
             ];
         } catch (Exception $e) {
             throw new RuntimeException($e->getMessage());
@@ -238,10 +284,10 @@ class OpenSsl
 
     /**
      * 对称解密
-     * @param  string  $encrypted  要解密字符
-     * @param  string  $key  加密秘钥
-     * @param  string  $iv
-     * @param  string  $encryptMethod  加密方法
+     * @param string $encrypted 要解密字符
+     * @param string $key 加密秘钥
+     * @param string $iv
+     * @param string $encryptMethod 加密方法
      * @return string
      * @author bonzaphp@gmail.com
      */
@@ -268,7 +314,32 @@ class OpenSsl
      */
     protected static function base64url_decode(string $input): string
     {
-        return base64_decode(strtr($input, '-_', '+/').str_repeat('=', 3 - (3 + strlen($input)) % 4));
+        return base64_decode(strtr($input, '-_', '+/') . str_repeat('=', 3 - (3 + strlen($input)) % 4));
     }
 
+    /**
+     * 转换pkcs格式公钥到pem格式
+     * @param string $pubKey
+     * @return string
+     * @author bonzaphp@gmail.com
+     */
+    public function pkcsToPemPublicKey(string $pubKey)
+    {
+        return "-----BEGIN PUBLIC KEY-----\n" .
+            wordwrap($pubKey, 64, "\n", true) .
+            "\n-----END PUBLIC KEY-----";
+    }
+
+    /**
+     * 转换pkcs格式私钥到pem格式
+     * @param string $private_key
+     * @return string
+     * @author bonzaphp@gmail.com
+     */
+    public function pkcsToPemPrivateKey(string $private_key)
+    {
+        return "-----BEGIN RSA PRIVATE KEY-----\n" .
+            wordwrap($private_key, 64, "\n", true) .
+            "\n-----END RSA PRIVATE KEY-----";
+    }
 }
